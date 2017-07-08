0cd14ed7009f59b2f091f1629ac840641df6760d
Airik Leon <airik.leon@gmail.com>
2017-07-08 06:23:41 -0500

    Add first 4 exercises

---
+++ FirstProg.java
@@ -0,0 +1,7 @@
+//Hello World
+public class FirstProg{
+  public static void main(String[] args) {
+    System.out.println("I,Airik Leon am determined to learn how to code.");
+    System.out.println("Today's date is 2017 07 07");
+  }
+}
